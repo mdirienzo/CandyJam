6a76bd7f58fcdbefeba8981907891083eb79f335
Michael DiRienzo <mdirienzo@cantinaconsulting.com>
2014-06-14 13:56:49 -0400

    Added current item and power stuff.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class PlayerManager : MonoBehaviour {
 
+	private ItemManager currentItem;
+	private ItemManager currentPower;
+
 	// Use this for initialization
 	void Start () {
-	
+		currentItem = null;
+		currentPower = null;
 	}
 	
 	// Update is called once per frame
@@ -15,10 +19,14 @@ public class PlayerManager : MonoBehaviour {
 
 
 	public void SetPlayerPower (ItemManager item) {
+		this.currentPower = item;
 		Debug.Log ("SetPlayerPower called with " + item.itemType);
 	}
 
 	public void SetPlayerItem (ItemManager item) {
+		this.currentItem = item;
 		Debug.Log ("SetPlayerPower called with " + item.itemType);
 	}
+
+
 }
