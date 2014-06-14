a28eb8c2cc794bb67f83b9b2892c8d3f5ed97ffb
Ross MacLeod <rmm-github@z.odi.ac>
2014-06-14 14:59:42 -0400

    add levelManager property to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    public LevelManager levelManager;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 
