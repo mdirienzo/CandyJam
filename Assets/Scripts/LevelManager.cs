307542eb5ebd3252154995ae20f444d1a9a021e8
natecope <natecope@gmail.com>
2014-06-14 12:41:31 -0400

    Added Game Manager and Level Manager. Also added example tile prefab

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Tile{
+	public GameObject prefab;
+}
+
+public class LevelManager : MonoBehaviour {
+	public Tile[] Tiles;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
