0752399f5ad6b373f1533213f6842b006c05d12c
Michael DiRienzo <mdirienzo@cantinaconsulting.com>
2014-06-14 14:31:29 -0400

    item managerz

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item {
+	public GameObject itemPrefab;
+}
+
+
+
+public class ItemManager : MonoBehaviour {
+
+	public enum ItemType { POWER_TYPE, ITEM_TYPE };
+	public ItemType itemType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
