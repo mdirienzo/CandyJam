f1a46353fd2546f10c64b01a1105cda3b0659783
natecope <natecope@gmail.com>
2014-06-15 11:53:20 -0400

    more animation things.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -6,19 +6,33 @@ public class InputController : MonoBehaviour {
 	public float movementSpeed;
 	private bool isMovable = true;
 
+	private Animator anim;
+	private bool isMoving = false;
+
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		if (rigidbody.velocity.magnitude == 0) {
+				isMoving = false;
+		} else {
+				isMoving = true;
+		}
+		anim.SetBool ("IsMoving", isMoving);
+		anim.SetFloat ("HorizontalSpeed", Input.GetAxis ("Horizontal"));
+		anim.SetFloat ("VerticalSpeed", Input.GetAxis ("Vertical"));
+
 		if (isMovable) {
+
 
 			if(Input.GetAxis("Horizontal") > 0){
 				//transform.Translate (Vector3.right * movementSpeed * Input.GetAxis("Horizontal"));
 				rigidbody.AddForce (Vector3.right * movementSpeed * Input.GetAxis("Horizontal"),ForceMode.Force);
+
 			}
 
 			if(Input.GetAxis("Horizontal") < 0){
